a04cf586563a54da03e34d7e82c19f97845860a3
Maxime Vercheval <maxime.vercheval@gmail.com>
2015-11-25 13:46:54 +0100

    indentation

--- inc/map.hh
+++ inc/map.hh
@@ -6,27 +6,27 @@
 
 enum case_type
 {
-    EMPTY = 0, //-
-    WHITE = 1, //O
-    BLACK = 2  //X
+	EMPTY = 0, //-
+	WHITE = 1, //O
+	BLACK = 2 //X
 };
 
 typedef struct __attribute__((__packed__)) case_field {
-  std::uint32_t data_black : 30;
-  std::uint32_t data_white : 30;
-  case_type occupancy : 2;
-  std::uint8_t referee : 2;
+	std::uint32_t data_black : 30;
+	std::uint32_t data_white : 30;
+	case_type occupancy : 2;
+	std::uint8_t referee : 2;
 } case_field;
 
 class Map
 {
 private:
-    case_field _map[19][19];
+	case_field _map[19][19];
 public:
-    Map();
-    case_type get_occ_case(unsigned int x, unsigned int y) const;
-    void set_occ_case(unsigned int x, unsigned int y, case_type content);
-    void print_occ_map() const;
+	Map();
+	case_type get_occ_case(unsigned int x, unsigned int y) const;
+	void set_occ_case(unsigned int x, unsigned int y, case_type content);
+	void print_occ_map() const;
 };
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -4,56 +4,58 @@
 
 Map::Map()
 {
-        memset(&_map, 0, sizeof(_map));
+	memset(&_map, 0, sizeof(_map));
 }
 
 case_type Map::get_occ_case(unsigned int x, unsigned int y) const
 {
-        if (x >= 19 || y >= 19) {
-                std::stringstream ss;
-                ss << "The position ("
-                   << x
-                   << ", "
-                   << y
-                   << ") is superior than (19, 19)";
-                throw std::out_of_range(ss.str());
-        }
-        return (_map[x][y].occupancy);
+	if (x >= 19 || y >= 19)
+	{
+		std::stringstream ss;
+		ss << "The position ("
+		   << x
+		   << ", "
+		   << y
+		   << ") is superior than (19, 19)";
+		throw std::out_of_range(ss.str());
+	}
+	return (_map[x][y].occupancy);
 }
 
 void Map::set_occ_case(unsigned int x, unsigned int y, case_type content)
 {
-        if (x >= 19 || y >= 19) {
-                std::stringstream ss;
-                ss << "The position ("
-                   << x
-                   << ", "
-                   << y
-                   << ") is superior than (19, 19)";
-                throw std::out_of_range(ss.str());
-        }
-        _map[x][y].occupancy = content;
+	if (x >= 19 || y >= 19)
+	{
+		std::stringstream ss;
+		ss << "The position ("
+		   << x
+		   << ", "
+		   << y
+		   << ") is superior than (19, 19)";
+		throw std::out_of_range(ss.str());
+	}
+	_map[x][y].occupancy = content;
 }
 
 void Map::print_occ_map() const
 {
-        for (int y = 0; y < 19; ++y)
-        {
-                for (int x = 0; x < 19; ++x)
-                {
-                        switch (get_occ_case(x, y))
-                        {
-                        case EMPTY:
-                                std::cout << "-";
-                                break;
-                        case WHITE:
-                                std::cout << "O";
-                                break;
-                        case BLACK:
-                                std::cout << "X";
-                                break;
-                        }
-                }
-                std::cout << std::endl;
-        }
+	for (int y = 0; y < 19; ++y)
+	{
+		for (int x = 0; x < 19; ++x)
+		{
+			switch (get_occ_case(x, y))
+			{
+			case EMPTY:
+				std::cout << "-";
+				break;
+			case WHITE:
+				std::cout << "O";
+				break;
+			case BLACK:
+				std::cout << "X";
+				break;
+			}
+		}
+		std::cout << std::endl;
+	}
 }
