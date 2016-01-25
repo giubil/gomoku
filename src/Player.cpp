9522ca78260b1dd4e1cce2d6541bf7af504121c2
Maxime Vercheval <maxime.vercheval@gmail.com>
2016-01-25 13:18:48 +0100

    fix player get_type

--- src/Player.cpp
+++ src/Player.cpp
@@ -31,7 +31,7 @@ std::tuple<int, int, bool> const * Player::play(Map const &map, Referee &ref, sf
     return (nullptr);
 }
 
-APlayer::player_type Player::get_player_type() const
+Player::player_type Player::get_player_type() const
 {
     return player_type::PLAYER;
 }
